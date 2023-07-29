df17c687e7c4643daaf0a5b47fc93c5fda706c6e
Milton <miltonrubro@gmail.com>
2023-07-28 21:28:58 -0300

    adicionando os exercicios

--- src/main/java/tech/ada/pokeada/PokeadaApplication.java
+++ src/main/java/tech/ada/pokeada/PokeadaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tech.ada.pokeada.model.Pokemon;
 import tech.ada.pokeada.repository.PokemonRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootApplication
@@ -26,5 +27,22 @@ public class PokeadaApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		/* Teste seu método aqui
 			repository.find... */
+		List<Pokemon> pokemonsDefenses = repository.findByDefenseIsGreaterThan(45);
+		System.out.println(pokemonsDefenses);
+		
+		List<Pokemon> pokemonsPsychic= repository.findByFirstTypeIn(List.of("psychic"));
+		System.out.println(pokemonsPsychic);
+		
+		List<Pokemon> pokemonsAttacks = repository.findBySpecialAttackBetween(60, 100);
+		System.out.println(pokemonsAttacks);
+		
+		List<Pokemon> pokemonsSpecials = repository.findBySpecialAttackIsGreaterThanEqual(33);
+		System.out.println(pokemonsSpecials);
+		
+		Optional<Pokemon> possiblePokemon = repository.findByName("mew");
+		if(possiblePokemon.isPresent()) {
+			System.out.println(possiblePokemon.get());
+		}
+
 	}
 }
--- src/main/java/tech/ada/pokeada/repository/PokemonRepository.java
+++ src/main/java/tech/ada/pokeada/repository/PokemonRepository.java
@@ -1,6 +1,10 @@
 package tech.ada.pokeada.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import tech.ada.pokeada.model.Pokemon;
 
@@ -18,7 +22,15 @@ public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
     * Escreva também buscas utilizando query nativa na interface PokemonRepository:
 
     * Obter Pokémon pelo nome.
+    * 
  * */
-
-
+    
+	 List<Pokemon> findByDefenseIsGreaterThan(Integer defense);
+	 List<Pokemon> findByFirstTypeIn(List<String> types);
+	 List<Pokemon> findBySpecialAttackIsGreaterThanEqual(Integer specialAttack);
+	 List<Pokemon> findBySpecialAttackBetween(Integer first, Integer second);
+	 
+	 @Query(value = "SELECT p FROM Pokemon p where p.name = :name")
+	 Optional<Pokemon> findByName(String name);
+	 
 }
